dc135f083a18ab3d31c3fe0fe96dc53c6b5ace7d
Michael Zhong <michael.zhong@t-mobile.com>
2017-12-21 08:37:47 -0800

    Connected with Firebase. Ready to create user.

--- app.js
+++ app.js
@@ -4,13 +4,39 @@ var firebase = require('firebase-admin');
 var logger = require('morgan');
 var path = require('path');
 var index = require('./route/index')
+var bodyParser = require('body-parser');
 
+//Openning connection to Firebase Database
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./configs/Project Air-4cd2d236ab04.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://project-air-34a3f.firebaseio.com"
+});
+
+//Connecting to Firebase Database
+/**
+var db = admin.database();
+var ref = db.ref("/users");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.key);
+}).catch(function (error){
+    console.log('Failed to get value from Firebase.', error)
+});**/
+    
 //View Engine Setup
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+app.set('firebase', admin);
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', index.view);
+app.get('/login', index.login);
+
+app.post('/register', index.register);
 
 app.listen(3000, () => console.log('App running on http://localhost:3000/.'));
--- route/index.js
+++ route/index.js
@@ -1,3 +1,22 @@
+
 exports.view = function(req, res, next){
     res.render('index');
+}
+
+exports.login = function(req, res, next){
+
+}
+
+exports.register = function(req, res, next){
+    res.render('loading');
+    var admin = require("firebase-admin");
+    console.log(req.body.reg_email);
+    admin.auth().getUserByEmail(req.body.reg_email)
+        .then(function(userRecord) {
+        // See the UserRecord reference doc for the contents of userRecord.
+        console.log("Successfully fetched user data:", userRecord.toJSON());
+    })
+        .catch(function(error) {
+        console.log("Error fetching user data:", error);
+    });
 }
